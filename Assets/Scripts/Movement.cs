d920f45b662a03523c0b065813d002f1da493999
Joshua Cory <accounts@keeplooking.net>
2022-10-09 17:49:23 -0600

    Added rocket sound

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -8,11 +8,13 @@ public class Movement : MonoBehaviour
     [SerializeField] float mainThrust = 100f;
     [SerializeField] float rotationThrust = 1f;
     Rigidbody rb;
+    AudioSource AudioSound;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();  
+        AudioSound = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -25,8 +27,17 @@ public class Movement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
+            if(!AudioSound.isPlaying)
+            {
+                AudioSound.Play();
+            }
+           
             rb.AddRelativeForce(UnityEngine.Vector3.up * mainThrust * Time.deltaTime);
         }
+         else
+            {
+                AudioSound.Stop();
+            }
     }
     void ProcessRotation()
     {
@@ -40,23 +51,11 @@ public class Movement : MonoBehaviour
         }
         void ApplyRotation(float rotationThisFrame)
     {
+        rb.freezeRotation = true; 
         transform.Rotate(UnityEngine.Vector3.forward * rotationThisFrame * Time.deltaTime);
+        rb.freezeRotation = false;  
     }
 
     }
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
