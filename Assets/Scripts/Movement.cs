88e434ecb0938f5c4c2d4a1626ae073bc824406c
Joshua Cory <accounts@keeplooking.net>
2022-10-09 10:25:41 -0600

    Added Transform.Rotate() and mainThrust

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-
+    [SerializeField] float mainThrust = 100f;
+    [SerializeField] float rotationThrust = 1f;
     Rigidbody rb;
 
     // Start is called before the first frame update
@@ -24,19 +25,24 @@ public class Movement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rb.AddRelativeForce(UnityEngine.Vector3.up);
+            rb.AddRelativeForce(UnityEngine.Vector3.up * mainThrust * Time.deltaTime);
         }
     }
     void ProcessRotation()
     {
          if (Input.GetKey(KeyCode.A))
         {
-            Debug.Log("left");
+            ApplyRotation(rotationThrust);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            Debug.Log("right");
+           ApplyRotation(-rotationThrust);
         }
+        void ApplyRotation(float rotationThisFrame)
+    {
+        transform.Rotate(UnityEngine.Vector3.forward * rotationThisFrame * Time.deltaTime);
+    }
+
     }
 
 
